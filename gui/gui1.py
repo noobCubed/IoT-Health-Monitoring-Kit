39d3cfbb0d0895ced4c2024edab132a484c186c0
Madhusudhan Sridhar <madhus.sri@gmail.com>
2018-12-23 14:39:55 +0530

    temp with records
    Kit finished for temperature sensor and measured values get updated in the records and csv file

--- gui/gui1.py
+++ gui/gui1.py
@@ -10,12 +10,16 @@ from kivy.uix.boxlayout import BoxLayout
 
 from kivy.factory import Factory
 from kivy.clock import Clock
-from kivy.properties import NumericProperty
+from kivy.properties import NumericProperty, StringProperty
 
 import os
 import glob
 import time
- 
+
+import csv
+import datetime
+import pandas as pd 
+
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
  
@@ -23,12 +27,32 @@ base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
 
-sensor_value = 0
+csvData = [['Timestamp', 'Farenheit']]
 
 class Root(BoxLayout):
     
-    sensor_value_c = NumericProperty(0)
     sensor_value_f = NumericProperty(0)
+    
+    rec1time = StringProperty('0')
+    rec2time = StringProperty('0')
+    rec3time = StringProperty('0')
+    rec4time = StringProperty('0')
+    rec5time = StringProperty('0')
+    rec6time = StringProperty('0')
+    rec7time = StringProperty('0')
+    rec8time = StringProperty('0')
+    rec9time = StringProperty('0')
+    rec10time = StringProperty('0')
+    rec1fahr = NumericProperty(0)
+    rec2fahr = NumericProperty(0)
+    rec3fahr = NumericProperty(0)
+    rec4fahr = NumericProperty(0)
+    rec5fahr = NumericProperty(0)
+    rec6fahr = NumericProperty(0)
+    rec7fahr = NumericProperty(0)
+    rec8fahr = NumericProperty(0)
+    rec9fahr = NumericProperty(0)
+    rec10fahr = NumericProperty(0)
     
     def mainPage(self):
         self.clear_widgets()
@@ -41,6 +65,13 @@ class Root(BoxLayout):
         self.add_widget(bPage)
         
     def tempPage(self):
+        try:
+            open('tempTest.csv')
+        except:    
+            with open('tempTest.csv', 'w', newline='') as csvFile:
+                writer = csv.writer(csvFile)
+                writer.writerows(csvData)
+        
         self.clear_widgets()
         tPage = Factory.TempPage()
         self.add_widget(tPage)
@@ -56,7 +87,6 @@ class Root(BoxLayout):
         self.add_widget(tInst)
         
     def tempMeasure(self):
-        
         self.sensor_value_c = 0
         self.sensor_value_f = 0
         
@@ -64,7 +94,7 @@ class Root(BoxLayout):
         tMeasure = Factory.TempMeasure()
         self.add_widget(tMeasure)
         
-        Clock.schedule_once(self.read_temp, 10)            
+        Clock.schedule_once(self.read_temp, 5)            
 
     def read_temp_raw(self):
         f = open(device_file, 'r')
@@ -82,8 +112,119 @@ class Root(BoxLayout):
             temp_string = lines[1][equals_pos+2:]
             self.sensor_value_c = float(temp_string) / 1000.0
             self.sensor_value_f = self.sensor_value_c * 9.0 / 5.0 + 32.0
-            
         
+        with open('tempTest.csv', 'a', newline='') as csvFile:
+            writer = csv.writer(csvFile)
+            now = str(datetime.datetime.now().replace(microsecond=0))
+            writer.writerow([now, self.sensor_value_f])
+        
+    def tempRecords(self):
+        self.genRecords()
+        
+        self.clear_widgets()
+        tRec = Factory.TempRecords()
+        self.add_widget(tRec) 
+        
+    def genRecords(self):
+        with open('tempTest.csv',"r") as f:
+            reader = csv.reader(f,delimiter = ",")
+            data = list(reader)
+            row_count = len(data)
+        
+        mdata = pd.read_csv('tempTest.csv',delimiter=',')
+        
+        if row_count <= 11:
+            try:
+                self.rec1time = str(mdata.iloc[0][0])
+                self.rec1fahr = str(mdata.iloc[0][1])
+            except:
+                pass
+            
+            try:
+                self.rec2time = str(mdata.iloc[1][0])
+                self.rec2fahr = str(mdata.iloc[1][1])
+            except:
+                pass
+            
+            try:
+                self.rec3time = str(mdata.iloc[2][0])
+                self.rec3fahr = str(mdata.iloc[2][1])
+            except:
+                pass
+            
+            try:
+                self.rec4time = str(mdata.iloc[3][0])
+                self.rec4fahr = str(mdata.iloc[3][1])
+            except:
+                pass
+            
+            try:
+                self.rec5time = str(mdata.iloc[4][0])
+                self.rec5fahr = str(mdata.iloc[4][1])
+            except:
+                pass
+            
+            try:
+                self.rec6time = str(mdata.iloc[5][0])
+                self.rec6fahr = str(mdata.iloc[5][1])
+            except:
+                pass
+            
+            try:
+                self.rec7time = str(mdata.iloc[6][0])
+                self.rec7fahr = str(mdata.iloc[6][1])
+            except:
+                pass
+            
+            try:
+                self.rec8time = str(mdata.iloc[7][0])
+                self.rec8fahr = str(mdata.iloc[7][1])
+            except:
+                pass
+            
+            try:
+                self.rec9time = str(mdata.iloc[8][0])
+                self.rec9fahr = str(mdata.iloc[8][1])
+            except:
+                pass
+            
+            try:
+                self.rec10time = str(mdata.iloc[9][0])
+                self.rec10fahr = str(mdata.iloc[9][1])
+            except:
+                pass
+        else:
+            
+            self.rec1time = str(mdata.iloc[row_count-11][0])
+            self.rec1fahr = str(mdata.iloc[row_count-11][1])
+            
+            self.rec2time = str(mdata.iloc[row_count-10][0])
+            self.rec2fahr = str(mdata.iloc[row_count-10][1])
+            
+            self.rec3time = str(mdata.iloc[row_count-9][0])
+            self.rec3fahr = str(mdata.iloc[row_count-9][1])
+            
+            self.rec4time = str(mdata.iloc[row_count-8][0])
+            self.rec4fahr = str(mdata.iloc[row_count-8][1])
+            
+            self.rec5time = str(mdata.iloc[row_count-7][0])
+            self.rec5fahr = str(mdata.iloc[row_count-7][1])
+            
+            self.rec6time = str(mdata.iloc[row_count-6][0])
+            self.rec6fahr = str(mdata.iloc[row_count-6][1])
+            
+            self.rec7time = str(mdata.iloc[row_count-5][0])
+            self.rec7fahr = str(mdata.iloc[row_count-5][1])
+            
+            self.rec8time = str(mdata.iloc[row_count-4][0])
+            self.rec8fahr = str(mdata.iloc[row_count-4][1])
+            
+            self.rec9time = str(mdata.iloc[row_count-3][0])
+            self.rec9fahr = str(mdata.iloc[row_count-3][1])
+            
+            self.rec10time = str(mdata.iloc[row_count-2][0])
+            self.rec10fahr = str(mdata.iloc[row_count-2][1])
+            
 class HealthguiApp(App):
     pass        
 
